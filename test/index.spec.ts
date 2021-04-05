4854a0bf49e4f85ccf8e4ce0daae29a213404e93
hiaux0 <hieudn5293@yahoo.de>
2021-04-06 00:59:19 +0200

    test(): correct check for disableLogger

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -6,7 +6,6 @@ let logger: Logger;
 
 function assertLogTrail(expectedLogTrail: string[]) {
   const logTrail = logger.getLogTrail();
-   logTrail/*?*/
   expect(logTrail.length).toEqual(expectedLogTrail.length);
 
   logTrail.forEach((logMessage, index) => {
@@ -19,9 +18,7 @@ function assertLogTrail(expectedLogTrail: string[]) {
 
 function assertMessages(content: string, _logger: Logger = logger) {
   const logTrail = _logger.getLogTrail();
-  logTrail/*?*/
   const hasContent = logTrail.some(logMessage => {
-   logMessage/*?*/
     const contentIncluded = logMessage.some(message => message.includes(content))
     return contentIncluded;
   })
@@ -44,7 +41,24 @@ describe('Logger - Options', () => {
     logger = new Logger({scope: SCOPE_NAME});
   })
 
-  xit('scope', () => {
+  describe('disableLogger', () => {
+    it('disableLogger - false', () => {
+      logger.setLogOptions({disableLogger:false});
+
+      logger.debug(['foo']);
+
+      assertMessages('foo');
+    });
+    it('disableLogger - true', () => {
+      logger.setLogOptions({disableLogger:true});
+
+      logger.debug(['foo']);
+
+      assertLogTrail([]);
+    });
+  });
+
+  it('scope', () => {
     logger.debug(['']);
 
     const formatedScopeName = `[${SCOPE_NAME}]`;
@@ -52,7 +66,7 @@ describe('Logger - Options', () => {
     assertMessages(formatedScopeName);
   });
 
-  xit('deepLogObjects', () => {
+  it('deepLogObjects', () => {
     logger.debug(['foo %o', {bar:'baz'}], {deepLogObjects:true});
 
     assertMessages('foo');
@@ -60,12 +74,12 @@ describe('Logger - Options', () => {
   });
 
   describe('dontLogUnlessSpecified', () => {
-    xit('dontLogUnlessSpecified - false', () => {
+    it('dontLogUnlessSpecified - false', () => {
       logger.debug(['foo'], {dontLogUnlessSpecified:false});
 
       assertMessages('foo');
     });
-    xit('dontLogUnlessSpecified - true', () => {
+    it('dontLogUnlessSpecified - true', () => {
       logger.debug(['foo'], {dontLogUnlessSpecified:true});
 
       assertLogTrail([]);
@@ -73,13 +87,13 @@ describe('Logger - Options', () => {
   });
 
   describe('focusedLogging', () => {
-    xit('focusedLogging - false', () => {
+    it('focusedLogging - false', () => {
       logger.setLogOptions({focusedLogging: false});
       logger.debug(['foo']);
 
       assertLogTrail(['foo']);
     })
-    xit('focusedLogging - true', () => {
+    it('focusedLogging - true', () => {
       logger.setLogOptions({focusedLogging: true});
       logger.debug(['foo']);
 
@@ -89,13 +103,13 @@ describe('Logger - Options', () => {
   });
 
   describe('throwOnError', () => {
-    xit('throwOnError - false', () => {
+    it('throwOnError - false', () => {
       logger.setLogOptions({throwOnError: false});
       logger.debug(['foo']);
 
       assertLogTrail(['foo']);
     })
-    // xit('throwOnError - true', () => {
+    // it('throwOnError - true', () => {
     //   logger.setLogOptions({throwOnError: true,isError:true});
     //   logger.debug(['foo']);
 
@@ -105,7 +119,7 @@ describe('Logger - Options', () => {
   });
 
   describe('prefix', () => {
-    xit('prefix - single', () => {
+    it('prefix - single', () => {
       logger.debug(['foo'],{prefix:1})
 
       assertMessages('foo');
@@ -114,7 +128,7 @@ describe('Logger - Options', () => {
 
       assertMessages(formatWithPrefix);
     });
-    fit('prefix - multiple', () => {
+    it('prefix - multiple', () => {
       logger.debug(['foo'],{prefix:1})
       logger.debug(['bar'],{prefix:2})
 
@@ -128,12 +142,16 @@ describe('Logger - Options', () => {
 });
 
 describe('Logger - Logging', () => {
-  xit('Should log messages - single', () => {
+  beforeEach(() => {
+    logger = new Logger({scope: SCOPE_NAME});
+  });
+
+  it('Should log messages - single', () => {
     logger.debug(['foo']);
 
     (assertMessages('foo'));
   });
-  xit('Should log messages - multiple', () => {
+  it('Should log messages - multiple', () => {
     logger.debug(['foo']);
     logger.debug(['bar']);
 
